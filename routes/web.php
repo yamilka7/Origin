a51a2a9826614bd379959492202c120dff0aa156
diana <dyc3@njit.edu>
2018-07-11 23:26:54 -0400

    generate auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ Route::get('/', 'PagesController@index')->name('index');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', 'PagesController@store')->name('contact.store');
 Route::get('/thanks/(name)', 'PagesController@thanks')->name('thanks');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
