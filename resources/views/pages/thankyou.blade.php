06a52ed61069b624f399d0c2de5f803f6d17b73f
diana <dyc3@njit.edu>
2018-07-16 21:40:33 -0400

    feature test to check if the register return a 200 status code.

--- resources/views/pages/thankyou.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.default')
-
-@section('content')
-    <h1>Thank you: {{$name}}</h1>
-
-    @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/about', 'PagesController@about')->name('about');
 Route::get('/', 'PagesController@index')->name('index');
 Route::get('/contact', 'PagesController@contact')->name('contact');
 Route::post('/contact', 'PagesController@store')->name('contact.store');
-Route::get('/thanks/(name)', 'PagesController@thanks')->name('thanks');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
